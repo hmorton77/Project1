52b9e85f94d0f24c89b30773f20219e0570445ff
hmorton77 <harold.m.morton@gmail.com>
2020-10-06 00:13:27 -0400

    map overhauled

--- js/button.js
+++ js/button.js
@@ -1,31 +1,22 @@
+$(".submitBtn").on("click", function (event) {
+  event.preventDefault();
+  var restaurantName = document.getElementById("restaurantName").value;
+  var cuisine = document.getElementById("cuisine").value;
+  var zipcode = document.getElementById("zipcode").value;
+  var carryout = document.getElementById("carryout").checked;
 
-$(".submitBtn").on("click", function(event){
-    event.preventDefault();
-    var restaurantName = document.getElementById("restaurantName").value;
-    var cuisine = document.getElementById("cuisine").value;
-    var zipcode = document.getElementById("zipcode").value;
-    var carryout = document.getElementById("carryout").checked;
+  console.log(restaurantName);
+  console.log(cuisine);
+  console.log(zipcode);
+  console.log(carryout);
 
-    console.log(restaurantName);
-    console.log(cuisine);
-    console.log(zipcode);
-    console.log(carryout);
-    
-    // if (restaurantName == true);
-        localStorage.setItem("restaurantName", restaurantName);
-    // if (cuisine == true);
-        localStorage.setItem("cuisine", cuisine);
-    // if (zipcode == true);
-        localStorage.setItem("zipcode", zipcode);
-    // if (carryout == true);
-        localStorage.setItem("carryout", carryout);
-    location.reload();
+  // if (restaurantName == true);
+  localStorage.setItem("restaurantName", restaurantName);
+  // if (cuisine == true);
+  localStorage.setItem("cuisine", cuisine);
+  // if (zipcode == true);
+  localStorage.setItem("zipcode", zipcode);
+  // if (carryout == true);
+  localStorage.setItem("carryout", carryout);
+  location.reload();
 });
-
-// step 1: Zomato API 
-
-// currency = "$"
-// var lat = 41.4993 //will be generated via function through google maps API
-// var lon = -81.6944 //will be generated via function through google maps API
-
-
--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,7 @@
+var map;
 function initMap() {
-    const myLatLng = { lat: 41.3886412, lng: -81.7294702 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng,
-    });
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
-    });
-  }
+  console.log("HATRED");
+  var uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  map = new google.maps.Map(document.getElementById("map"), { zoom: 4, center: uluru });
+}
